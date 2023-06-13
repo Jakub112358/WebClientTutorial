ebbc658e15774f50d48805af777fcf106627d03c
Jakub Szymański <jakubszymanski@hotmail.com>
2023-06-13 20:57:41 +0200

    Initial commit

---
+++ src/main/java/com/js/webclienttutorial/WebClientTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.js.webclienttutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebClientTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebClientTutorialApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/js/webclienttutorial/WebClientTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.js.webclienttutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebClientTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
